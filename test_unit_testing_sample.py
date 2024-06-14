6067450b15e2becdc808b492f0421e10aa8dfd19
Mohsin Shah <Mohsin.Shah@gosh.nhs.uk>
2024-06-14 13:12:20 +0100

    updated with some unit tests

--- test_unit_testing_sample.py
+++ test_unit_testing_sample.py
@@ -1,37 +1,43 @@
 import pytest
+import unit_testing_sample as uts
 
-
-# Samples
+# General Tests
 
 def func(x):
     return x + 1
 
-
 def test_answer():
     assert func(3) == 4
 
-def test_structure_starts_empty():
-    assert structure starts empty
-def test_empty_structure():
-    assert structure is None
+# Testing with heaps
 
-def test_structure_not_empty():
-    assert strucure is not empty
+def test_create_empty_list():
+    assert uts.create_empty_list()==[]
 
-def test_structure_inserts():
-    assert struture inserts
+def test_add_values_list_and_create_heap():
+    assert len(uts.add_values_list_and_create_heap()) == 9
 
-def test_struture_appends():
-    assert structure appends
+def test_add_to_heap():
+    assert len(uts.add_to_heap()) == 10
 
-def test_structure_pops():
-    assert structure pops
-
-
-# Arguments
-def test_default_structure_argument():
-    assert default_arg_output
-
-def test_manual_input_argument():
-    assert manual_input_arg
-
+#
+# def test_structure_not_empty():
+#     assert strucure is not empty
+#
+# def test_structure_inserts():
+#     assert struture inserts
+#
+# def test_struture_appends():
+#     assert structure appends
+#
+# def test_structure_pops():
+#     assert structure pops
+#
+#
+# # Arguments
+# def test_default_structure_argument():
+#     assert default_arg_output
+#
+# def test_manual_input_argument():
+#     assert manual_input_arg
+#
---
+++ unit_testing_sample.py
@@ -0,0 +1,24 @@
+# importing "heapq" to implement heap queue
+import heapq
+
+
+def create_empty_list():
+    # initializing list
+    main_list = []
+    return main_list
+
+
+def add_values_list_and_create_heap():
+    main_list = create_empty_list()
+    main_list.extend([1001, 199, 1002, 1003, 10, 12, 200, 1004, 1005])
+    # using heapify to convert list into heap
+    heapq.heapify(main_list)
+    return main_list
+#
+# # printing created heap
+# print ("The created heap is : ",(list(li)))
+
+def add_to_heap():
+    main_list = add_values_list_and_create_heap()
+    heapq.heappush(main_list, 14)
+    return main_list
